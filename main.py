fba3d5ef512e524e388aeef5ba5c63a6fb7e2c10
Zefyruz101 <mdfaris2006@outlook.com>
2024-04-22 19:19:15 +1000

    Added the accessibility menu

--- main.py
+++ main.py
@@ -20,16 +20,26 @@ splash_root.overrideredirect(True)
 splash_label = Label(splash_root, text="Welcome")
 splash_label.pack(pady=10)
 
+def open_mammals_lesson():
+    pass
+
+def open_birds_lesson():
+    pass
+
+def open_fish_lesson():
+    pass
+
+
 #Main program
 def main_window():
     splash_root.destroy()
 
     root = Tk()
-    root.title('Animal Lesson App')
+    root.title('All About Animals App')
 
     #Setting basic variables for fonts, and window sizing
     app_width = 500
-    app_height = 500 
+    app_height = 400 
 
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -39,10 +49,35 @@ def main_window():
 
     root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
-    Program_title_label = Label(root, text="Click on a lesson to begin!")
-    Program_title_label.pack
+    menubar = Menu(root)
+    root.config(menu=menubar)
 
-    
+    # Accessibility Menu
+    accessibility_menu = Menu(menubar, tearoff=False)
+    menubar.add_cascade(label="Accessibility Settings", menu=accessibility_menu)
+
+    # Font Type submenu
+    font_menu = Menu(accessibility_menu, tearoff=False)
+    accessibility_menu.add_cascade(label="Font Type", menu=font_menu)
+
+    # Dark Mode and Light Mode
+    theme_menu = tk.Menu(accessibility_menu, tearoff=False)
+    accessibility_menu.add_cascade(label="Theme", menu=theme_menu)
+
+    Program_title_label = Label(root, text="Click on a lesson to begin!")
+    Program_title_label.pack()
+
+    mammals_button = Button(root, text="Lesson on Mammals", width = 20 , command=open_mammals_lesson)
+    mammals_button.pack(pady=20)
+
+    birds_button = Button(root, text="Lesson on Birds", width = 20 , command=open_birds_lesson)
+    birds_button.pack(pady=20)
+
+    fish_button = Button(root, text="Lesson on Fish", width = 20 , command=open_fish_lesson)
+    fish_button.pack(pady=20)
+
+
+ 
 
 #Splash screen countdown
 splash_root.after(2000, main_window)
