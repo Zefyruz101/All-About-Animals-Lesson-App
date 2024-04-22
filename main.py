f0823e989759e78d8f3e1a021cdc1e70a48b415e
Zefyruz101 <mdfaris2006@outlook.com>
2024-04-22 21:31:48 +1000

    Added an image as a background for the main menu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 # Splash screen
 splash_root = Tk()
@@ -21,7 +22,19 @@ splash_label = Label(splash_root, text="Welcome")
 splash_label.pack(pady=10)
 
 def open_mammals_lesson():
-    pass
+    mlesson_root = Tk()
+    mlesson_root.title('Mammal Lesson')
+
+    app_width = 700
+    app_height = 700 
+
+    screen_width = mlesson_root.winfo_screenwidth()
+    screen_height = mlesson_root.winfo_screenheight()
+
+    x = (screen_width / 2) - (app_width / 2)
+    y = (screen_height / 2) - (app_height / 2)
+
+    mlesson_root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
 def open_birds_lesson():
     pass
@@ -38,8 +51,8 @@ def main_window():
     root.title('All About Animals App')
 
     #Setting basic variables for fonts, and window sizing
-    app_width = 500
-    app_height = 400 
+    app_width = 900
+    app_height = 570 
 
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
@@ -48,6 +61,14 @@ def main_window():
     y = (screen_height / 2) - (app_height / 2)
 
     root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
+
+    #image_path = PhotoImage(file=r"C:\Users\mdfar\Downloads\SDD Y12 Image folder\animals pic.jpg")
+    #bg_image = Label(root, image=image_path)
+    #bg_image.place(relheight=1, relwidth=1)
+    my_img = ImageTk.PhotoImage(Image.open("animals pic.png"))
+    root.my_img = my_img
+    bg_image = Label(image=my_img)
+    bg_image.place(relheight=1, relwidth=1)
 
     # Accessibility Menu
     menubar = Menu(root)
@@ -67,14 +88,18 @@ def main_window():
     Program_title_label.pack()
 
     #Lesson Buttons
-    mammals_button = Button(root, text="Lesson on Mammals", width = 20 , command=open_mammals_lesson)
+    mammals_button = Button(root, text="Lesson on Mammals", width=20, command=open_mammals_lesson)
     mammals_button.pack(pady=20)
 
-    birds_button = Button(root, text="Lesson on Birds", width = 20 , command=open_birds_lesson)
+    birds_button = Button(root, text="Lesson on Birds", width=20, command=open_birds_lesson)
     birds_button.pack(pady=20)
 
-    fish_button = Button(root, text="Lesson on Fish", width = 20 , command=open_fish_lesson)
+    fish_button = Button(root, text="Lesson on Fish", width=20, command=open_fish_lesson)
     fish_button.pack(pady=20)
+
+
+
+
 
 
  
