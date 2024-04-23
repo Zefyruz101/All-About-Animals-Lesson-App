b1eeb63dd21803230a2a7695341f566f9b29a73a
Zefyruz101 <mdfaris2006@outlook.com>
2024-04-23 13:19:27 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from customtkinter import *
+from tkinter import *
+import customtkinter
 from PIL import Image
 
 # Splash screen
-splash_root = CTk()
+splash_root = customtkinter.CTk()
 
 app_width = 500
 app_height = 500 
@@ -18,11 +19,11 @@ splash_root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 #Hide title bar
 splash_root.overrideredirect(True)
 
-splash_label = CTkLabel(master=splash_root, text="Welcome")
+splash_label = customtkinter.CTkLabel(master=splash_root, text="Welcome")
 splash_label.pack(pady=10)
 
 def open_mammals_lesson():
-    mlesson_root = CTk()
+    mlesson_root = customtkinter.CTk()
     mlesson_root.title('Mammal Lesson')
 
     app_width = 1000
@@ -37,17 +38,17 @@ def open_mammals_lesson():
     mlesson_root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
     #Back Button
-    back_button = CTkButton(master=mlesson_root, text="Back", command=mlesson_root.destroy)
+    back_button = customtkinter.CTkButton(master=mlesson_root, text="Back", command=mlesson_root.destroy)
     back_button.pack(side="top", anchor="nw")
 
-    mammal_lesson = CTkLabel(master=mlesson_root, text="I love mammal" )
+    mammal_lesson = customtkinter.CTkLabel(master=mlesson_root, text="I love mammal" )
     mammal_lesson.pack()
 
-    mammal_quiz_button = CTkButton(master=mlesson_root, text="Quiz", command=mammal_quiz)
+    mammal_quiz_button = customtkinter.CTkButton(master=mlesson_root, text="Quiz", command=mammal_quiz)
     mammal_quiz_button.pack()
 
 def open_birds_lesson():
-    blesson_root = CTk()
+    blesson_root = customtkinter.CTk()
     blesson_root.title('Bird Lesson')
 
     app_width = 1000
@@ -62,17 +63,17 @@ def open_birds_lesson():
     blesson_root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
     #Back Button
-    back_button = CTkButton(master=blesson_root, text="Back", command=blesson_root.destroy)
+    back_button = customtkinter.CTkButton(master=blesson_root, text="Back", command=blesson_root.destroy)
     back_button.pack(side="top", anchor="nw")
 
-    bird_lesson = CTkLabel(master=blesson_root, text="I love bird" )
+    bird_lesson = customtkinter.CTkLabel(master=blesson_root, text="I love bird" )
     bird_lesson.pack()
 
-    bird_quiz_button = CTkButton(master=blesson_root, text="Quiz", command=bird_quiz)
+    bird_quiz_button = customtkinter.CTkButton(master=blesson_root, text="Quiz", command=bird_quiz)
     bird_quiz_button.pack()
 
 def open_fish_lesson():
-    flesson_root = CTk()
+    flesson_root = customtkinter.CTk()
     flesson_root.title('Fish Lesson')
 
     app_width = 1000
@@ -87,13 +88,13 @@ def open_fish_lesson():
     flesson_root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
 
     #Back Button
-    back_button = CTkButton(master=flesson_root, text="Back", command=flesson_root.destroy)
+    back_button = customtkinter.CTkButton(master=flesson_root, text="Back", command=flesson_root.destroy)
     back_button.pack(side="top", anchor="nw")
 
-    fish_lesson = CTkLabel(master=flesson_root, text="I love fish") 
+    fish_lesson = customtkinter.CTkLabel(master=flesson_root, text="I love fish") 
     fish_lesson.pack()
 
-    fish_quiz_button = CTkButton(master=flesson_root, text="Quiz", command=fish_quiz)
+    fish_quiz_button = customtkinter.CTkButton(master=flesson_root, text="Quiz", command=fish_quiz)
     fish_quiz_button.pack()
 
 def mammal_quiz():
@@ -109,7 +110,7 @@ def fish_quiz():
 def main_window():
     splash_root.destroy()
 
-    root = CTk()
+    root = customtkinter.CTk()
     root.title('All About Animals')
 
     #Setting basic variables for fonts, and window sizing
@@ -126,8 +127,9 @@ def main_window():
 
    
     my_img = Image.open("bgimg.png")
+    my_img = customtkinter.CTkImage(my_img)
     #root.my_img = my_img
-    bg_image = CTkLabel(master=root, image=my_img)
+    bg_image = customtkinter.CTkLabel(master=root, image=my_img)
     bg_image.place(relheight=1, relwidth=1)
 
     # Accessibility Menu
@@ -144,17 +146,17 @@ def main_window():
    # theme_menu = Menu(accessibility_menu, tearoff=False)
     #accessibility_menu.add_cascade(label="Theme", menu=theme_menu)
 
-    Program_title_label = CTkLabel(master=root, text="Click on a lesson to begin!")
+    Program_title_label = customtkinter.CTkLabel(master=root, text="Click on a lesson to begin!")
     Program_title_label.pack()
 
     #Lesson Buttons
-    mammals_button = CTkButton(master=root, text="Lesson on Mammals", width=20, command=open_mammals_lesson)
+    mammals_button = customtkinter.CTkButton(master=root, text="Lesson on Mammals", width=20, command=open_mammals_lesson)
     mammals_button.pack(pady=20)
 
-    birds_button = CTkButton(master=root, text="Lesson on Birds", width=20, command=open_birds_lesson)
+    birds_button = customtkinter.CTkButton(master=root, text="Lesson on Birds", width=20, command=open_birds_lesson)
     birds_button.pack(pady=20)
 
-    fish_button = CTkButton(master=root, text="Lesson on Fish", width=20, command=open_fish_lesson)
+    fish_button = customtkinter.CTkButton(master=root, text="Lesson on Fish", width=20, command=open_fish_lesson)
     fish_button.pack(pady=20)
 
 
@@ -162,4 +164,3 @@ def main_window():
 splash_root.after(2000, main_window)
 
 splash_root.mainloop()
-
